d9a3fc8b49a161e8c9ea63c8bdff93a61c378245
Ethan <ktun7@mail.ccsf.edu>
2016-07-26 10:12:51 -0700

    made major changes

--- DrawGUI.java
+++ DrawGUI.java
@@ -1,4 +1,4 @@
-package Project6;
+
 
 //Project by: Ryan Lim,	Ethan <INSERT NAME HERE>
 //, and Marion Fiona Gallagher.
@@ -9,29 +9,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class DrawGUI extends JFrame{
+public class DrawGUI extends JPanel{
 
 private JRadioButton pen_1, pen_2, pen_3, eraser;
 private JButton clearButton;
 private static boolean canDraw;
-
+private final static int DIAMETER = 10;
+private ArrayList<Point> myPoints;
 public DrawGUI() {
-	super("Welcome to Drawing!");
-	setSize(600, 600);
 	
-	Container canvas = getContentPane();
-	canvas.setBackground(Color.PINK);
-	canvas.addMouseListener(new CanvasListener());
+	//super("Welcome to Drawing!");
+	//setSize(600, 600);
 	
-	JPanel mainPanel = new JPanel();
-	mainPanel.setBackground(Color.GRAY);
-	canvas.setLayout(new BorderLayout());
-	canvas.add(mainPanel, BorderLayout.PAGE_END);
+	//Container canvas = getContentPane();
+	setBackground(Color.pink);
+	myPoints = new ArrayList<Point>();
+
+	
+	//JPanel mainPanel = new JPanel();
+	//mainPanel.setBackground(Color.GRAY);
+	//canvas.setLayout(new BorderLayout());
+	//canvas.add(mainPanel, BorderLayout.PAGE_END);
 	
 	JPanel drawPanel = new JPanel();
+	ButtonGroup myGroup = new ButtonGroup();
+	
 	pen_1 = new JRadioButton("Color option 1!");
 	drawPanel.add(pen_1);
 	pen_1.addActionListener(new ToolListener());
@@ -41,6 +47,12 @@ public DrawGUI() {
 	pen_3 = new JRadioButton("Color option 3!");
 	drawPanel.add(pen_3);
 	pen_3.addActionListener(new ToolListener());
+	
+	myGroup.add(pen_1);
+	myGroup.add(pen_2);
+	myGroup.add(pen_3);
+	
+
 	eraser = new JRadioButton("Eraser");
 	drawPanel.add(eraser);
 	eraser.addActionListener(new ToolListener());
@@ -48,7 +60,10 @@ public DrawGUI() {
 	drawPanel.add(clearButton);
 	clearButton.addActionListener(new ToolListener());
 	
-	mainPanel.add(drawPanel);
+	this.add(drawPanel);
+	
+	this.addMouseMotionListener(new CanvasListener());
+	this.addMouseListener(new CanvasListener());
 	canDraw = false;
 }
 
@@ -61,18 +76,50 @@ public static void setCanDraw (boolean canIDraw) {
 	canDraw = canIDraw;
 }
 
+@Override
+public void paintComponent(Graphics pen) {
+	super.paintComponent(pen);
 
+		for(Point point : myPoints) {
+			int x = (int) point.getX();
+			int y = (int) point.getY();
+			
+				pen.setColor(Color.blue);
+				pen.fillOval(x, y, DIAMETER, DIAMETER);
+			
+			
+	}
+	
+}
 private class CanvasListener extends MouseAdapter {
 	@Override
 	public void mouseClicked (MouseEvent event){
 		if (canDraw == true) {
 			canDraw = false;
+			
 		} else if (canDraw == false) {
 			canDraw = true;
+			repaint();;
 		}
 		System.out.println(canDraw);
+		
+		
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent event) {
+		 
+			if(canDraw)
+			{
+				Point point = event.getPoint();
+				myPoints.add(point);
+			}
+			
+			repaint();
 	}
 }
+
+
 
 private class ToolListener implements ActionListener {
 	@Override
@@ -85,7 +132,11 @@ public static void main(String args[]) {
 	System.out.println("running");
 	EventQueue.invokeLater(new Runnable() {
 		public void run() {
-			DrawGUI frame = new DrawGUI();
+			JFrame frame = new JFrame("Welcome to Drawing!");
+			frame.setSize(600, 600);
+			// create an object of your class
+			DrawGUI panel = new DrawGUI();
+			frame.getContentPane().add(panel);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 		}
